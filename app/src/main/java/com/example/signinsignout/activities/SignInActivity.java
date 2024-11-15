6695ce09cf94d93074da0c2b67ecb549a11e1bf2
Aaron Snyder <snyderaar@cwu.edu>
2024-11-14 23:03:29 -0800

    Commit with custom app icon and login success message

--- app/src/main/java/com/example/signinsignout/activities/SignInActivity.java
+++ app/src/main/java/com/example/signinsignout/activities/SignInActivity.java
@@ -84,6 +84,7 @@ public class SignInActivity extends AppCompatActivity {
 
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        showToast("Login successful");
                         startActivity(intent);
                     } else {
                         loading(false);
